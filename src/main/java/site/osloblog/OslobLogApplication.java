9749059aa1e3911c30d20646f0fe6aa14952bc74
조경훈 <kh.cho@okestro.com>
2024-05-20 13:34:01 +0900

    Initial commit

---
+++ src/main/java/site/osloblog/OslobLogApplication.java
@@ -0,0 +1,13 @@
+package site.osloblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OslobLogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OslobLogApplication.class, args);
+  }
+
+}
---
+++ src/test/java/site/osloblog/OslobLogApplicationTests.java
@@ -0,0 +1,13 @@
+package site.osloblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OslobLogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
